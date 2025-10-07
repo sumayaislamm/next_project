f5f02e0da8e663e0192a8e2f1f5b359afb41f61d
Sumaya <sheikhsumaya622@gmail.com>
2025-10-07 23:50:57 +0600

    reviews list

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -8,4 +8,4 @@ export default async function Reviewdetails({params}:
     <h1>Of the product of {productId}</h1>
     </>
     );
-}
+} 
---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,14 @@
+export default function Reviews(){
+    return (
+<>
+<h1>
+    The Lists of reviews
+</h1>
+<li>Product 01</li>
+<li>Product 02</li>
+<li>Product 03</li>
+<li>Product 04</li>
+<li>Product 05</li>
+</>
+    );
+}
