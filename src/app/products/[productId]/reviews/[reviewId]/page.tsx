daed3c0bcc59ef7a6812614a928d76bee351ecf7
Sumaya <sheikhsumaya622@gmail.com>
2025-10-07 23:32:40 +0600

    Nested Dynamic Routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,11 @@
+export default async function Reviewdetails({params}:
+  {params : Promise <{ productId : string, reviewId : string; }>}
+){
+    const {productId, reviewId} = await params;
+    return (
+    <>
+    <h1>Review of {reviewId}</h1>
+    <h1>Of the product of {productId}</h1>
+    </>
+    );
+}
